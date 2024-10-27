742939b674f06d2d7a5685cb3e490b0010fac2d6
Carlos Anselmi <anselmi.dev@gmail.com>
2024-10-26 22:55:32 -0300

    FIXED - getUrl to photo

--- resources/views/livewire/pages/gallery.blade.php
+++ resources/views/livewire/pages/gallery.blade.php
@@ -1,7 +1,7 @@
 <x-containers.content>
     <div class="relative columns-2 sm:columns-3 lg:columns-4 gap-4 lg:gap-8 space-y-4">
         @foreach ($photos as $photo)
-            <x-gallery.card class="aspect-w-16 aspect-h-9" src="{{ $photo->cover->getUrl('thumb') }}"/>
+            <x-gallery.card class="aspect-w-16 aspect-h-9" src="{{ $photo->cover?->getUrl('thumb') }}"/>
         @endforeach
 
         <!-- <a href="blog-single.html" class="block text-dark font-bold text-xl mb-3.5">
